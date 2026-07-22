93ae5f47519202720e4441ab4d06dcc6a9dd7f06
x21y <377137055@qq.com>
2026-07-22 23:30:27 +0000

    update yxs.js.

--- yxs.js
+++ yxs.js
@@ -1,7 +1,7 @@
 import cheerio from"cheerio.min.js";import"crypto-js.js";import"cLFE.js";import"kOUW.js";import"ucoN.js";import 模板 from"XUKQ.js";import{gbkTool}from"wYCz.js";function init_test(){console.log(FAAAAAASUVORK5CYII=`;OcrApi.api=OCR_API;let code=OcrApi.classification(img_base64);log("测试验证码图片的ocr识别结果为:"+code)}function rsa_demo_test(){let t1=(new Date).getTime();let pkcs1_public=`
 02606169929d7:07,fuer,0260617071722,02606186513d7:33,02606197:035960d,02606207:16,3818d,0260623072405,5018d20260627070/02606300723黑短,02607040708,黑,5099d,02607060735,2200d,黑短,
 02607070707, 5960d,黑,02607071807,26车,02607090710,黑,x7688,02607140722，黑，5960d，02607150711 ，
-白短qun,5960d,02607210715,黑,x7688,02607220731,5960d,极地
+白短qun,5960d,02607210715,黑,x7688,02607220731,5960d,极地,02607230716,5960d,黑,
 -----BEGIN RSA PUBLIC KEY-----
 MEgCQQCrI0pQ/ERRpJ3Ou190XJedFq846nDYP52rOtXyDxlFK5D3p6JJu2RwsKwy
 lsQ9xY0xYPpRZUZKMEeR7e9gmRNLAgMBAAE=
